26ecd9a2edf6aa2c005b9deb31fa4b74bd0fa8e1
eragon996 <f2015467@goa.bits-pilani.ac.in>
2018-01-29 17:07:56 +0530

    mergeSort added

--- Sort.java
+++ Sort.java
@@ -40,6 +40,37 @@ public class Sort {
         }
     }
 
+    public void mergeSort(in[] arr, int p, int r){
+        if(p >=r ) return;
+        int mid = (p+r)/2;
+        mergeSort(arr, p, q);
+        mergeSort(arr, q+1, r);
+        merge(arr,p,q,r);
+    }
+
+    public void merge(int[] arr, int p ,int q, int r){
+        int n1 = q-p+1, n2 = r-q;
+        int[] B = new int[n1];
+        int[] C = new int[n2];
+        for(int i = p ; i <= q ;i++){
+            B[i-p]=arr[i];
+        }
+        for(int i = q+1 ; i <= r ;i++){
+            B[i-q-1]=arr[i];
+        }
+        int i=0,j=0;
+        for( int k = p ; k < r ;k++){
+            if(B[i] <= C[j]) {
+                arr[k-1]=B[i];
+                i++;
+            }else {
+                arr[k] = C[j];
+                j++;
+            }
+        }
+
+    }
+
     public void printArray(int[] arr){
         for(int i = 0 ; i <arr.length ; i++)
             System.out.printf("%d ",arr[i]);
