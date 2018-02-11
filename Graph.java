57e843e5100761139d30ac1c39b2f3e50acf4ce7
eragon996 <f2015467@goa.bits-pilani.ac.in>
2018-02-11 13:13:47 +0530

    Connected Component

--- Graph.java
+++ Graph.java
@@ -51,6 +51,31 @@ public class Graph {
             repost.push(v);
         }
     }
+    public void stronglyConnectedComponent(){
+        /**
+         * Kosaraju dfs(twice)
+         */
+
+    }
+    public void connectedComponen(ArrayList<ArrayList<Integer>> adj){
+        int v = adj.size();
+        boolean[] marked = new boolean[v];
+        int[] id = new int[v];
+        int count  = 0;
+        for(int i = 0 ; i < v ; i++){
+            if(!marked[i]){
+                cc_DFS(adj, marked, id, count, i);
+                count++;
+            }
+        } 
+    }
+    public void cc_DFS( ArrayList<ArrayList<Integer>> adj, boolean[] marked,int[] id,int count,int v ){
+        marked[v]=true;
+        id[v] = count;
+        for(int w : adj.get(v)){
+            if(!marked[w]) ts_DFS(adj, marked, repost, w);
+        }
+    }
     public ArrayList<ArrayList<Integer>> DAG_adjList(int[][] arr){
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
